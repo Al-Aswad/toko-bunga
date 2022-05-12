6839ea4583744cfe944b41312fe8903387d7c723
Hajar Aswad <hajaraswadkom@gmail.com>
2022-05-12 22:07:51 +0700

    layouts admin

---
+++ application/views/layouts/admin/v_main.php
@@ -0,0 +1,149 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>AdminLTE 3 | Dashboard</title>
+
+     <?php $this->load->view('includes/v_styles') ?>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed text-sm">
+     <div class="wrapper">
+
+          <!-- Preloader -->
+          <div class="preloader flex-column justify-content-center align-items-center">
+               <img class="animation__shake" src="assets/admin/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+          </div>
+
+          <!-- Navbar -->
+          <?php $this->load->view('includes/v_navbar') ?>
+          <!-- /.navbar -->
+          <style>
+               .bg__gradient {
+                    /* ff 3.6+ */
+                    background: -moz-linear-gradient(90deg, rgba(41, 50, 60, 1) 0%, rgba(45, 55, 66, 1) 100%);
+
+                    /* safari 5.1+,chrome 10+ */
+                    background: -webkit-linear-gradient(90deg, rgba(41, 50, 60, 1) 0%, rgba(45, 55, 66, 1) 100%);
+
+                    /* opera 11.10+ */
+                    background: -o-linear-gradient(90deg, rgba(41, 50, 60, 1) 0%, rgba(45, 55, 66, 1) 100%);
+
+                    /* ie 6-9 */
+                    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#2D3742', endColorstr='#29323C', GradientType=0);
+
+                    /* ie 10+ */
+                    background: -ms-linear-gradient(90deg, rgba(41, 50, 60, 1) 0%, rgba(45, 55, 66, 1) 100%);
+
+                    /* global 94%+ browsers support */
+                    background: linear-gradient(90deg, rgba(41, 50, 60, 1) 0%, rgba(45, 55, 66, 1) 100%);
+               }
+
+               /* sidebar */
+               /* .sidebar-mini .main-sidebar .nav-link,
+			.sidebar-mini-md .main-sidebar .nav-link,
+			.sidebar-mini-xs .main-sidebar .nav-link {
+				width: calc(220px - .5rem * 2);
+			}
+
+			.main-sidebar,
+			.main-sidebar::before {
+				width: 220px;
+			}
+
+			.layout-fixed .brand-link {
+				width: 220px;
+			}
+
+			
+
+			@media (min-width: 768px) {
+
+				body:not(.sidebar-mini-md):not(.sidebar-mini-xs):not(.layout-top-nav) .content-wrapper,
+				body:not(.sidebar-mini-md):not(.sidebar-mini-xs):not(.layout-top-nav) .main-footer,
+				body:not(.sidebar-mini-md):not(.sidebar-mini-xs):not(.layout-top-nav) .main-header {
+					transition: margin-left .3s ease-in-out;
+					margin-left: 250px;
+				}
+			} */
+               .sidebar-dark-primary .nav-sidebar>.nav-item>.nav-link.active,
+               .sidebar-light-primary .nav-sidebar>.nav-item>.nav-link.active {
+                    background-color: #2f7ccf96;
+               }
+
+               /* .sidebar-dark-primary .nav-sidebar>.nav-item>.nav-link.active,
+               .sidebar-light-primary .nav-sidebar>.nav-item>.nav-link.active {
+
+               } */
+
+               [class*=sidebar-dark-] .nav-treeview>.nav-item>.nav-link.active,
+               [class*=sidebar-dark-] .nav-treeview>.nav-item>.nav-link.active:focus,
+               [class*=sidebar-dark-] .nav-treeview>.nav-item>.nav-link.active:hover {
+                    background-color: rgb(56 61 69);
+                    color: #eee;
+               }
+
+               [class*=sidebar-dark-] .sidebar a {
+                    color: #8fa0bf;
+               }
+          </style>
+          <!-- Main Sidebar Container -->
+          <aside class="main-sidebar sidebar-dark-primary bg__gradient text-sm">
+               <!-- Brand Logo -->
+               <a href=" index3.html" class="py-4 brand-link text-sm border-0">
+                    <img src=" assets/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                    <span class="brand-text font-weight-light">AdminLTE 3</span>
+               </a>
+
+               <!-- Sidebar -->
+               <?php $this->load->view('includes/admin/v_sidebar') ?>
+               <!-- /.sidebar -->
+          </aside>
+
+          <!-- Content Wrapper. Contains page content -->
+          <div class="content-wrapper">
+               <!-- Content Header (Page header) -->
+               <div class="content-header">
+                    <div class="container-fluid">
+                         <div class="row mb-2">
+                              <div class="col-sm-6">
+                                   <h1 class="m-0">Dashboard</h1>
+                              </div><!-- /.col -->
+                              <div class="col-sm-6">
+                                   <ol class="breadcrumb float-sm-right">
+                                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                        <li class="breadcrumb-item active">Dashboard v1</li>
+                                   </ol>
+                              </div><!-- /.col -->
+                         </div><!-- /.row -->
+                    </div><!-- /.container-fluid -->
+               </div>
+               <!-- /.content-header -->
+
+               <!-- Main content -->
+               <section class="content">
+                    <div class="container-fluid">
+                         <!-- Small boxes (Stat box) -->
+                         <?php $this->load->view('pages/admin/v_dashboard') ?>
+                    </div>
+               </section>
+
+               <!-- /.content -->
+          </div>
+          <!-- footer -->
+          <?php $this->load->view('includes/v_footer') ?>
+
+     </div>
+     <!-- Scripts -->
+     <?php $this->load->view('includes/v_scripts') ?>
+
+</body>
+
+</html>
---
+++ application/views/layouts/v_login.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>AdminLTE 3 | Log in</title>
+
+     <!-- Google Font: Source Sans Pro -->
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+     <!-- Font Awesome -->
+     <link rel="stylesheet" href="assets/admin/plugins/fontawesome-free/css/all.min.css">
+     <!-- icheck bootstrap -->
+     <link rel="stylesheet" href="assets/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+     <!-- Theme style -->
+     <link rel="stylesheet" href="assets/admin/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition login-page">
+     <div class="login-box text-sm">
+          <div class="login-logo">
+               <a href="assets/admin/index2.html"><b>Silahkan Login !</b></a>
+          </div>
+          <!-- /.login-logo -->
+          <div class="card">
+               <div class="card-body login-card-body">
+                    <p class="login-box-msg">Masukkan username dan password</p>
+
+                    <form action="assets/admin/index3.html" method="post">
+                         <div class="input-group mb-3">
+                              <input type="email" class="form-control form-control-sm" placeholder="Email" autocomplete="off">
+                              <div class="input-group-append">
+                                   <div class="input-group-text">
+                                        <span class="fas fa-envelope"></span>
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="input-group mb-3">
+                              <input type="password" class="form-control form-control-sm" placeholder="Password" autocomplete="off">
+                              <div class="input-group-append">
+                                   <div class="input-group-text">
+                                        <span class="fas fa-lock"></span>
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="form-check ml-1">
+                              <input type="checkbox" class="form-check-input" id="exampleCheck2">
+                              <label class="form-check-label" for="exampleCheck2">Remember me</label>
+                         </div>
+                         <a href="#" class="btn btn-block btn-primary mt-3 mb-2">
+                              <i class="fab fa-facebook mr-2"></i> Login
+                         </a>
+                    </form>
+
+                    <!-- <p class="mb-1">
+                         <a href="forgot-password.html">I forgot my password</a>
+                    </p>
+                    <p class="mb-0">
+                         <a href="register.html" class="text-center">Belum punya akun?, Registrasi</a>
+                    </p> -->
+               </div>
+               <!-- /.login-card-body -->
+          </div>
+     </div>
+     <!-- /.login-box -->
+
+     <!-- jQuery -->
+     <script src="assets/admin/plugins/jquery/jquery.min.js"></script>
+     <!-- Bootstrap 4 -->
+     <script src="assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+     <!-- AdminLTE App -->
+     <script src="assets/admin/dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
